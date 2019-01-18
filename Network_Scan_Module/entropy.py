c8c65978c2cbc3928ffa672bb1c47b7f3a482d1e
ak5690@naver.com <fb1778879>
2019-01-18 11:35:32 +0900

    packet_sniff gan ji nam

--- Network_Scan_Module/entropy.py
+++ Network_Scan_Module/entropy.py
@@ -4,11 +4,11 @@ from collections import Counter
 def calculate_entropy(symbol_list):
     entropy = 0
     total_symbol_count = float(len(symbol_list))
-    values_symbol = Counter(symbol_list).values() # counts the elements' frequency
+    values_symbol = Counter(symbol_list) # counts the elements' frequency
 
-    for symbol_count in values_symbol:
+    for symbol_count in values_symbol.values():
         percentage = symbol_count/total_symbol_count
         reverse_percentage = total_symbol_count/symbol_count
         entropy += percentage * math.log(reverse_percentage,2)
 
-    return entropy
+    return {"total_symbol_count":total_symbol_count,"values_symbol":values_symbol,"entropy":entropy}
--- Network_Scan_Module/packet_sniff.py
+++ Network_Scan_Module/packet_sniff.py
@@ -3,6 +3,10 @@ import numpy
 from . import entropy
 
 def packet_sniff(time):
+
+    max_ent_rate : 5
+    min_ent_rate : 1
+
     cap=pyshark.LiveCapture(interface='eth0',only_summaries="IP" "protocol")
     cap.sniff(timeout=time)
     pkts = [str(pkt).split(" ")[2:5] for pkt in cap._packets]
@@ -11,4 +15,38 @@ def packet_sniff(time):
     victim_entropy = entropy.calculate_entropy(analytical_resource[0])
     attack_entropy = entropy.calculate_entropy(analytical_resource[1])
     
-    print("victim_rate = {0}, attack_rate = {1}".format(victim_entropy,attack_entropy))
+    print("---------- Victim_Status ----------")
+    symbol = victim_entropy["values_symbol"]
+    max_symbol = max(symbol,key=symbol.get)
+    ent = victim_entropy["entropy"]
+    print("total_symbol_count = {count}\n"
+          "The number of IPs that were found the most = {IP} : {IP_count}"
+          "\nEntropy rate = {ent}".format(
+        count=victim_entropy["total_symbol_count"],
+        IP = max_symbol,
+        IP_count = symbol[max_symbol],
+        ent = ent))
+
+    if ent <= 1 or ent >= 5:
+        print("You are Victim!")
+    else :
+        print("You are not Victim")
+
+    print("---------- Attack_Status ----------")
+    symbol = attack_entropy["values_symbol"]
+    max_symbol = max(symbol,key=symbol.get)
+    ent = attack_entropy["entropy"]
+    print("total_symbol_count = {count}\n"
+          "The number of IPs that were found the most = {IP} : {IP_count}"
+          "\nEntropy rate = {ent}".format(
+        count=attack_entropy["total_symbol_count"],
+        IP = max_symbol,
+        IP_count = symbol[max_symbol],
+        ent = ent))
+
+    if ent <= 1 or ent >= 5:
+        print("You are Attacker!")
+    else :
+        print("You are not Attacker")
+
+    print("\nPacket Test ends....")
